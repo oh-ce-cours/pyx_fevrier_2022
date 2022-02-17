b29624f930bbb03bb3ac323dee32cca4638ac3d2
Matthieu Falce <matthieu@falce.net>
2022-02-17 17:14:20 +0100

    gitautosync update 190

--- tape_en_cours/ex_poo.py
+++ tape_en_cours/ex_poo.py
@@ -1,4 +1,4 @@
 p1 = Personne(naissance=1990, nom="Matthieu")
 p2 = Personne(naissance=2015, nom="Paul")
-print(p1.est_majeur() == True)
-print(p2.est_majeur() == False)
+# print(p1.est_majeur() == True)
+# print(p2.est_majeur() == False)
