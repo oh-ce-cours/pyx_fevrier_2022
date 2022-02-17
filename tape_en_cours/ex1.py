d7ca79e8144c508fb3330b3ed9defd3d7a7e1c73
Matthieu Falce <matthieu@falce.net>
2022-02-17 10:54:51 +0100

    gitautosync update 92

--- tape_en_cours/ex1.py
+++ tape_en_cours/ex1.py
@@ -1,12 +1,13 @@
 # from utils import traitement_cellule
 # traitement_cellule()
 
-# import utils 
+# import utils
 # utils.traitement_cellule()
 
-import time 
-# on veut transformer un "CSV séparé par des lignes" en 
-# "CSV avec des largeurs de colonnes fixes (5 chars) en mettant au carré chacun des éléments" 
+import time
+
+# on veut transformer un "CSV séparé par des lignes" en
+# "CSV avec des largeurs de colonnes fixes (5 chars) en mettant au carré chacun des éléments"
 ligne = "  1,   2   ,3,4,5 \n ,  toto, 7, 9, 10, 11"
 datas = ligne.split(",")
 
@@ -14,21 +15,17 @@ data_sans_espaces = []
 for element in datas:
     try:
         time.sleep(0.5)
-    except Exception: 
+    except Exception:
         print("on sort pas")
 
-
-
-
-
-    element_sans_espace =     element.strip()  # on enleve les espaces
+    element_sans_espace = element.strip()  # on enleve les espaces
     try:
         element_au_carre = str(int(element_sans_espace) ** 2)
     except ValueError:
         element_au_carre = "NA"
     data_sans_espaces.append(element_au_carre)
 
-    ## différentes méthodes d'affichage, par affinité avec le formateur 
+    ## différentes méthodes d'affichage, par affinité avec le formateur
     ## elles font toutes la même chose
     print(f"'{element_au_carre}'")
     # print("'{}'".format(element_sans_espace))
