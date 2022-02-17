cf99f44c24fdf416b6164de883da98d40aa9d601
Matthieu Falce <matthieu@falce.net>
2022-02-17 10:50:25 +0100

    gitautosync update 88

---
+++ tape_en_cours/utils.py
@@ -0,0 +1,12 @@
+def traitement_cellule(element):
+    element_sans_espace = element.strip()  # on enleve les espaces
+    try:
+        element_transforme = float(1 / int(element_sans_espace)) ** 2
+        element_au_carre = f"{element_transforme:.2f}"
+    except ValueError:
+        element_au_carre = "NA"
+    except ArithmeticError as e:
+        print("On a un 0, on ne devrait pas", e, type(e))
+        element_au_carre = "NaN"
+
+    return f'{element_au_carre:>8}'
