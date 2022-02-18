9be595ebb0d1fe1b73e4a6b07c15f8439e8be701
Matthieu Falce <matthieu@falce.net>
2022-02-18 16:39:08 +0100

    gitautosync update 286

--- tape_en_cours/exemple_installation/recovered/recovered.py
+++ tape_en_cours/exemple_installation/recovered/recovered.py
@@ -31,9 +31,6 @@ def main(tar_file, check):
 
 
 def recover(tar_file, check):
-    import pdb
-
-    pdb.set_trace()
     untar(tar_file, extracted)
     for path in extracted.iterdir():
         correct_extention = puremagic.from_file(str(path))
