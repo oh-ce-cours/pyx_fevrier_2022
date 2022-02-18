1c0946477f4904dd621ffc2f9a31eb47b3b061bb
Matthieu Falce <matthieu@falce.net>
2022-02-18 15:33:41 +0100

    gitautosync update 270

--- tape_en_cours/example_tk.py
+++ tape_en_cours/example_tk.py
@@ -7,7 +7,7 @@ frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
 
-widget = tkinter.Tk()
+widget = Tk()
 mask = tkinter.READABLE | tkinter.WRITABLE
 widget.tk.createfilehandler(file, mask, callback)
 
