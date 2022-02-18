8af4dc21d1cb1e859d06cbcaafeb3d54d71a740f
Matthieu Falce <matthieu@falce.net>
2022-02-18 14:56:45 +0100

    gitautosync update 232

--- tape_en_cours/recovered_click.py
+++ tape_en_cours/recovered_click.py
@@ -2,21 +2,8 @@ from logging import root
 from pathlib import Path
 import tarfile
 import puremagic
-import argparse
+import click
 import shutil
-
-
-parser = argparse.ArgumentParser(description="Recover some files.")
-parser.add_argument("path", type=str, help="the .tar(.gz) path to extract")
-parser.add_argument(
-    "--check",
-    action="store_true",
-    help="Should the actions be perfomed",
-)
-
-args = parser.parse_args()
-tar_file = args.path
-check = args.check
 
 
 def untar(tar_filename, extract_path):
@@ -32,26 +19,29 @@ def untar(tar_filename, extract_path):
 
 extracted = Path(".") / "extracted"
 recovered = Path(".") / "recovered"
-untar(tar_file, extracted)
-for path in extracted.iterdir():
-    correct_extention = puremagic.from_file(str(path))
-    if "jfif" in correct_extention:
-        correct_extention = ".jpg"
 
-    extension_without_point = correct_extention[1:]
-    new_name = path.parts[-1].split(".")[0]
-    new_name = f"{new_name}.{extension_without_point}"
-    new_file_path = recovered / extension_without_point / new_name
-    print(f"{path} => {new_file_path}")
+
+def main():
+    untar(tar_file, extracted)
+    for path in extracted.iterdir():
+        correct_extention = puremagic.from_file(str(path))
+        if "jfif" in correct_extention:
+            correct_extention = ".jpg"
+
+        extension_without_point = correct_extention[1:]
+        new_name = path.parts[-1].split(".")[0]
+        new_name = f"{new_name}.{extension_without_point}"
+        new_file_path = recovered / extension_without_point / new_name
+        print(f"{path} => {new_file_path}")
+        if not check:
+            new_file_path.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copyfile(path, new_file_path)
+        else:
+            print(f"should create {new_file_path.parent}")
+
     if not check:
-        new_file_path.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copyfile(path, new_file_path)
+        shutil.make_archive("recovered", format="zip", root_dir=recovered)
+        shutil.rmtree(recovered)
+        shutil.rmtree(extracted)
     else:
-        print(f"should create {new_file_path.parent}")
-
-if not check:
-    shutil.make_archive("recovered", format="zip", root_dir=recovered)
-    shutil.rmtree(recovered)
-    shutil.rmtree(extracted)
-else:
-    print("should create archive")
+        print("should create archive")
