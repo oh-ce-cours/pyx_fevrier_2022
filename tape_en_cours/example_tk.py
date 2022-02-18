f95a88b9dab522a427a63b1da2731224cea965e7
Matthieu Falce <matthieu@falce.net>
2022-02-18 15:30:05 +0100

    gitautosync update 267

--- tape_en_cours/example_tk.py
+++ tape_en_cours/example_tk.py
@@ -1,26 +1,9 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
