210211d9e109c5e5ddff95de76716ecd248626e6
Matthieu Falce <matthieu@falce.net>
2022-02-18 14:44:00 +0100

    gitautosync update 222

--- tape_en_cours/recovered.py
+++ tape_en_cours/recovered.py
@@ -11,7 +11,7 @@ parser.add_argument("path", type=str, help="the .tar(.gz) path to extract")
 parser.add_argument(
     "--check",
     dest="accumulate",
-    action="store_const",
+    action="store_true",
     const=sum,
     default=max,
     help="Should the actions be perfomed",
